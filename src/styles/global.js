8e813d0789b2dc81c6f6bf2fefb3aa3430ff46b3
Tuanemendes <tuane.belmiro@gmail.com>
2023-06-13 23:44:23 -0300

    configuração do projeto

---
+++ src/styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+    background-color: #f0f0f0;
+  }
+`;
+
+export default GlobalStyles;
