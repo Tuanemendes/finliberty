df10d3887260565e902e7bfe240017a588f578c0
Tuanemendes <tuane.belmiro@gmail.com>
2023-06-15 23:15:24 -0300

    centralizar texto

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,9 +7,12 @@ export const Container = styled.div`
     background-color: #20295f;
     border-bottom: 5px solid #ee6b26;
     display: flex;
+
 `;
 
-export const Header = styled.h1``;
+export const Header = styled.h1`
+    width: 100%;
+`;
 
 export const Title = styled.div`
     padding-top: 10px;
